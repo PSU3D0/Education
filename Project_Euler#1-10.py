d27692392181cb6ce1d3674e4140c9254f23992f
PSU3D0 <frankie.colson@gmail.com>
2018-12-02 15:20:14 -0800

    TODO: fix #10 Start #11

--- Project_Euler#1-10.py
+++ Project_Euler#1-10.py
@@ -1,5 +1,102 @@
 #Project Euler - Problems 1-10
 
+#Summation of Primes | #10
+#https://projecteuler.net/problem=10
+#Going for an actual sieve this time 
+
+import math as m
+
+#Creating boolean dict sequenced by nums 2-maxN
+maxN = 2000000
+l1 = dict()
+for x in range(2,maxN):
+    l1[x] = True
+#Instantiating logic variables
+j = 0
+ctr = 1
+
+#Count loop
+ct = list(range(2,int(m.sqrt(maxN))))
+#Sieve
+for num in ct:
+    if l1[num] == True:
+        for key in l1.keys():
+            if key % num == 0 and key != num:
+                l1[key] = False
+    
+#Summation
+Sum = 0
+for i in l1.keys():
+    if l1[i] == True:
+        Sum += i
+print(Sum)
+
+#Executes in about 22 seconds, aka not efficient
+#Just learned about enumerate() replaces all this dict nonsense
+#Will fix in next commit
+
+
+
+
+'''
+# Special Pythagorean Triplet | #9
+#https://projecteuler.net/problem=9
+for a in range(0,1000):
+    for b in range(a,1000-a):
+        c = 1000 - a - b
+        if a * a + b * b == c*c:
+            print((a,b,c))
+            print((a+b+c))
+            print((a*b*c))
+#Pythonic much?
+'''
+
+'''
+#Largest Product in a Series | #8
+#https://projecteuler.net/problem=8
+
+num = str("""73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450""".replace("\n", ""))
+
+store = 0
+
+for a in num:
+    indexA = num.index(a)
+    a = int(a)
+    if indexA < 986:
+
+        i = 1
+        determine = a
+        while i < 12:
+        
+            determine *= int(num[indexA+i])
+            i += 1
+        if determine > store:
+            store = determine
+print(store)
+'''
+
+
+
+'''
 #10001st Prime | #7
 #https://projecteuler.net/problem=7
 
@@ -13,14 +110,8 @@ while len(primes) <= limit:
     primes.append(p)
     if len(l1) != 1:
         p = min(l1)
-print(primes[limit])
-
-
-    
-        
-        
-
-
+print(primes[limit])9
+'''
 
 '''
 #Sum Square Difference | #6
